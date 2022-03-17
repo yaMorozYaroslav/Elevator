00ed7b3090176f05fcd2c9136f0f197ec871768d
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-17 20:54:42 +0100

    36

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+
+export function App(){
+	return(
+     <div className="App">
+       <header className="App-header">
+          <h1>React Google Login App</h1>
+         <div>
+          <GoogleLogin></GoogleLogin>
+         </div>
+        </header>
+        </div>
+		)
+}
