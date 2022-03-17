5a124f9756711fabdaee5e7b72cb9daf2a742375
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-17 21:04:47 +0100

    17

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 
 export function App(){
+  const handleFailure = (result) => {
+  	alert(result);
+  };
+   const handleLogin = (googleData) => {
+   	console.log(googleData);
+   };
 	return(
      <div className="App">
        <header className="App-header">
           <h1>React Google Login App</h1>
          <div>
-          <GoogleLogin></GoogleLogin>
+          <GoogleLogin
+        clientId={process.env/REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleFailure}
+        cooliePolicy={'single_host_origin'}
+          ></GoogleLogin>
          </div>
         </header>
         </div>
