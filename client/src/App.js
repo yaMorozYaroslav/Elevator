e1e8ae895effac24d90e3327b7a0bf4b9cc0d938
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-18 20:58:42 +0100

    43

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+
+
+export function App(){
+  const [place, setElev] = React.useState([]);
+  React.useEffect(()=>{
+    const one = {floor: '1'};
+    axios.put('https://localhost/8080/floors')
+  })
+}
