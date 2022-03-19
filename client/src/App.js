1c4477c3c0948a4979889e54f2aadd6e5b324c91
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-19 09:53:40 +0100

    43

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import axios from 'axios';
+const baseUrl = 'http://localhost:8080/';
+const 
 
 
 export function App(){
   const [place, setElev] = React.useState([]);
   React.useEffect(()=>{
-    const one = {floor: '1'};
-    axios.put('https://localhost/8080/floors')
+    axios.put('http://localhost:8080/floor/3')
+         .then(response=>setElev(response.data.place))
   })
+  return <p>{JSON.stringify(place)}</p>
 }
