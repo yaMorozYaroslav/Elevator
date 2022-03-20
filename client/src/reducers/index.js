aae790b00c899b84f27b111f1db32517cff36fc4
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-20 22:33:06 +0100

    44

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,13 @@
 let defaultState={floor: ''}
+
+const mainReducer=(state=defaultState,action)=>{
+	if(action.type==="SEE_FLOOR"){
+		return{
+			...state,
+			floor:action.floor
+		}
+	}else{
+		return{...state}
+	}
+}
+export default mainReducer;
