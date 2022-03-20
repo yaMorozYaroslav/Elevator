b86d23d4b0e88cc449340de71196b360c20f31fd
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-20 22:43:29 +0100

    39

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index.js';
+import Boxcon from './cont/Boxcon';
 import {render} from 'react-dom';
+
+let store = createStore(reducers, applyMiddleware(thunk))
+
 import {App} from './App';
-render(	<App />, document.getElementById('root')
+render(	<Provider store={store}><App /></Provider>, document.getElementById('root')
 );
 
