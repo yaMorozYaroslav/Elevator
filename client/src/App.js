58bfa4dcb9d25c86235165bd137aedbecd10b72c
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-21 15:28:07 +0100

    54

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import {Contr} from './components/Contr/Contr'
 import {fetcher} from './Res.js';
 export function App(){
   const [place, setPlace] = React.useState([]);
+  const [click, setClick] = React.useState(false);
+  const handClick = () => setClick(!click);
   React.useEffect(()=>{
  fetcher().then(response=>setPlace(response));
   }, [place])
-  return (<><p>{JSON.stringify(place)}</p><Contr/></>);
+  return (<><p>{JSON.stringify(place)}</p>
+            <button onClick={handClick}>Click</button>
+            <Contr/></>);
 }
