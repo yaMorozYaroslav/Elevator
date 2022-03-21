7e72b6d226883eb7a11c6a495a8248c72f58d29c
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-21 16:33:11 +0100

    49

--- client/src/Depp.js
+++ client/src/Depp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-export function Button(){
+function Buston(){
 	const [click, setClick] = React.useState(false);
     const handClick = () => setClick(!click);
     
@@ -9,9 +9,8 @@ export function Button(){
 	axios.put("http://localhost:8080/floor/1")
 	     .then((res)=>console.log(value));
 	
-	     },[click]);
-	return <>
-	<button ref={refer} onClick={handClick}>1</button>
-    </>
+	     },[click, handClick]);
+	return click;
 }
 
+export const Button =()=><button onClick={handClick}>1</button>
