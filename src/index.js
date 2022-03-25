f17b85e39dd6692941fed81f30fdedc5898f736c
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-03-25 20:39:53 +0100

    50

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ app.use("/", (req, res) => {
   res.status(404).send({ error: "Resource not found" });
 });
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.statec('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
