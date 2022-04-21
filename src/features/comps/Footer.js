aaaca3593685eaf40fc054a4ae267e307e8a0a98
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-21 15:31:34 +0200

    056

--- src/features/comps/Footer.js
+++ src/features/comps/Footer.js
@@ -1,25 +1,42 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 import {RemainingTodos,
         StatusFilter, 
         ColorFilters} from '../filters/FiltComps'
 
 import {onColorChange, onStatusChange} from '../filters/filtersSlice'
-
+import {
+  completedTodosCleared,
+  allTodosCompleted
+} from '../todos/todosSlice'
 export const Footer =()=>{
+  const dispatch = useDispatch()
+
 	const todosRemaining = useSelector(state=>{
 		const uncompletedTodos = state.todos.filter(todo=>!todo.completed)
 		return uncompletedTodos.length
 	})
-	const {status, colors} = useSelector(state=>state.filters)
+	const {status, colors} = useSelector((state)=>state.filters)
+
+  const onMarkCompletedClicked =()=> dispatch(allTodosCompleted())
+  const onClearCompletedClicked =()=> dispatch(completedTodosCleared())
+
+  const colorChange =(color, changeType)=>
+                        dispatch(onColorChange(color, changeType))
+  const statusChange =(status)=>
+                        dispatch(onStatusChange(status))
 
 	return(
       <footer className="footer">
         <div className="actions">
           <h5>Actions</h5>
-          <button className="button">Mark All Comleted</button>
-          <button className="button">Clear Completed</button>
+          <button 
+             className="button"
+             onClick={onMarkCompletedClicked}>Mark All Comleted</button>
+          <button 
+             className="button"
+             onClick={onClearCompletedClicked}>Clear Completed</button>
          </div>
 
          <RemainingTodos count={todosRemaining}/>
--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector, shallowEqual} from 'react-redux'
 import {TodoItem} from './TodoItem'
 
 const selectTodoIds = state => state.todos.map(todo=>todo.id)
 
 export const TodoList =()=>{
-	const todoIds = useSelector(selectTodoIds)
+	const todoIds = useSelector(selectTodoIds, shallowEqual)
 
 	const renderedListItems = todoIds.map(todoId=>{
 		return <TodoItem key={todoId} id={todoId}/>
