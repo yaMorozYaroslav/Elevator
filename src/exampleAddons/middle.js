838f7e13a0080bc691f8844710557fc8d2c70d55
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-23 19:48:46 +0200

    22

--- src/exampleAddons/middle.js
+++
@@ -1,8 +0,0 @@
-export const loggerMiddleware = storeAPI => next => action =>{
-	if(action.type === 'todos/todoAdded'){
-		setTimeout(()=>{
-			console.log('Added a new todo', action.payload)
-		}, 4000)
-	}
-	return next(action)
-}
