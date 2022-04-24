c6619572c829de1bab30097bdfebc20bc9bafe65
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-24 10:56:27 +0200

    98

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -25,46 +25,37 @@ const PostExcerpt = ({post}) => {
       </article>
     )
 }
-
-export const PostsList = () => {
+export const PostsList =()=>{
   const dispatch = useDispatch()
-
   const posts = useSelector(selectAllPosts)
+
   const postStatus = useSelector(state => state.posts.status)
+  const error = useSelector(state => state.posts.error)
 
   useEffect(()=>{
     if(postStatus === 'idle'){
       dispatch(fetchPosts())
     }
-  }, [postStatus,dispatch])
+  }, [postStatus, dispatch])
 
-  // Sort posts in reverse chronological order by datetime string
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
+  let content
+  if (postStatus === 'loading'){
+    content = <Spinner text="Loading..." />
+  } else if(postStatus === 'succeeded'){
+    const orderedPosts = posts
+       .slice()
+       .sort((a, b)=>b.date.localeCompare(a.date))
 
-  const renderedPosts = orderedPosts.map((post) => {
-    return (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
-    )
-  })
-
-  return (
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+      ))
+  } else if(postStatus === 'failed'){
+    content = <div>{error}</div>
+  }
+  return(
     <section className="posts-list">
-      <h2>Posts</h2>
-      {renderedPosts}
-    </section>
-  )
+     <h2>Posts</h2>
+     {content}
+     </section>
+    )
 }
