d2e737203b66df4207a2b902edfa94b1e4c4071b
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-26 20:26:24 +0200

    48

--- src/App.js
+++ src/App.js
@@ -10,10 +10,18 @@ import {useSelector, useDispatch} from 'react-redux'
 import {Navbar} from './app/Navbar'
 import {List} from './atoms/sides/List'
 import {selectAllSides} from './atoms/sides/sidesSlice'
+import {fetchSides} from './atoms/sides/sidesSlice'
 //import {Elevs} from
 
 export function App(){
-
+	const dispatch = useDispatch()
+	const sides = useSelector(selectAllSides)
+	const sideStatus = useSelector(state=>state.sides.status)
+  React.useEffect(()=>{
+     if(sideStatus === 'idle'){ 
+     	dispatch(fetchSides())}
+	}, [sideStatus, dispatch])
+	
 	return(
       <Router>
        <Routes>
