eea3f7f50ee0349be134fbd4ffa5101451b31e6a
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-27 07:58:28 +0200

    82

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-
-import sidesReducer from '../atoms/sides/sidesSlice'
-
-export default configureStore({
-	reducer: {
-		sides: sidesReducer
-	}
-})
