94b14ecd49795a806536d8670b76331edc7ab9f5
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-27 23:14:26 +0200

    587

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import AddSide from './comps/AddSide'
+//import List from './comps/List'
+import './styles.css'
+
+export function App(){
+	return(
+      <div>
+        <h1>List</h1>
+        </div>
+		)
+}
