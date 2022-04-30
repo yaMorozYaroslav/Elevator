65ea3d0e274e93b1e9b87c123d04288cdc8f9fe2
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-30 08:31:14 +0200

    02#

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,6 @@
-export const getTodos = store =>
-  getTodoList(store).map(id => getTodoById(store, id));
 export const selectAllSides = state => state.sides.sides
 
 export const selectSideById =(state, sideId)=>
              state.sides.sides.find(side => side.id === sideId)
-             
+export const getAllSides = state =>
+  selectAllSides(state).map(id => selectSideById(state, id));
