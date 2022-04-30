8a02b3086592c683e2dd3531d9b20434b14ea385
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-04-30 08:27:27 +0200

    49

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,12 +1,3 @@
-export const getTodoList = store =>
-  store && store.todos ? store.todos.allIds : [];
-
-export const getTodoById = (store, id) =>
-  store && store.todos && store.todos.byIds
-    ? { ...store.todos.byIds[id], id }
-    : {};
-
-
 export const getTodos = store =>
   getTodoList(store).map(id => getTodoById(store, id));
 export const selectAllSides = state => state.sides.sides
