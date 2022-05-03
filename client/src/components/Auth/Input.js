7e954cb5f7017dd3b0e39876d55147b5155d6a21
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-03 20:28:09 +0200

    42

--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -14,7 +14,7 @@ const Input =({name, handleChange, label, half, autoFocus, type, handleShowPassw
                    label={label} 
                    autoFocus={autoFocus}
                    type={type}
-                   InputProps={name==='password'&&{
+                   InputProps={name==='password'?{
                       endAdornment:(
                        <InputAdornment position="end">
                          <IconButton onClick={handleShowPassword}>
@@ -22,7 +22,7 @@ const Input =({name, handleChange, label, half, autoFocus, type, handleShowPassw
                          </IconButton>
                        </InputAdornment>
                    		)
-                   }} />
+                   }:null} />
                 </Grid>
  )
 export default Input
