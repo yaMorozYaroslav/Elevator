aab103bb344ce120ae88f16036495e8090de6f17
Yaroslav <ya.moroz.yaroslav@gmail.com>
2022-05-06 21:06:37 +0200

    756

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -13,11 +13,24 @@ import {deletePost, likePost} from '../../../actions/postact'
 export const Post =({post, setCurrentId})=>{
 	const classes = useStyles()
 	const dispatch = useDispatch()
+
+    const Likes = () => {
+    	if(post.likes.length > 0){
+    	return post.likes.find((like)=>like===(user?.result?.googleId||user?.result?._id))
+          ?(
+            <><ThumbUpAltIcon fontSize="small" />&nbsp; {post.likes.length > 2? `You and ${post.likes.length -1} others`: `${post.likes.length} like${post.likes.length>1?'s':''}`}</>
+          	):(
+             <><ThumbUpAltOutlined fontSize="small" />&nbsp; {post.likes.length} {post.likes.length === 1?'Like':'Likes'}</>
+          	)
+    	}
+    	return <><ThumbUpAltOutlined fontSize="small" />&nbsp:Like</>
+    }
+
 	return(
         <Card className={classes.card}>
            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
 		<div className={classes.overlay}>
-		    <Typography variant="h6">{post.creator}</Typography>
+		    <Typography variant="h6">{post.name}</Typography>
 		    <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
 		</div>
 		<div className={classes.overlay2}>
