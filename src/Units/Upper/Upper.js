561c34bffb4ec4103d8ec60eb0fd2265ce0b52e2
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2023-02-07 08:49:07 -0400

    016

--- src/Units/Upper/Upper.js
+++ src/Units/Upper/Upper.js
@@ -2,23 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 export function Upper(){
-	const [state, setState] = React.useState(null)
-	
-	const handleChange0 = () => {state===null?setState(0):setState(null)}
-	const handleChange1 = () => {state===null?setState(1):setState(null)}
-	
+	const [state, setState] = React.useState({click0: null, click1: null})
+	const newValue = {click0: 0}
+	const handleChange0 = () => {state.click0===null
+		                         ?setState(state => ({...state, ...newValue}))
+		                         :setState(state => ({...state, click0: null}))
+							 }
+	const handleChange1 = () => {state===null?setState({click1:0}):setState({click1:null})}
+	console.log(state.click0)
+	//background: ${props => props.state !== null ? props.state === 1 ? 'green' : 'grey': 'blue'}
 	const Box = styled.section`
 	width:100px;
 	height:100px;
-	background: ${props => props.state !== null ? props.state === 1 ? 'green' : 'grey': 'blue'}
+	
 	`
 	
 	return(
 	<>
-	<Box state={state}/>
-	<h1>{state === null ? state === 0 ? 'grey box' : 'blue box' : 'green box'}</h1>
-	<Area disabled={state === 1} numb={state} onClick={handleChange0} onDoubleClick={null}/>
-	<Area disabled={state === 0} numb={state} onClick={handleChange1}/>
+	<Box state={state.click0}/>
+	<h1>{state.clickState0 === null ? state.clickState0 === 0 ? 'grey box' : 'blue box' : 'green box'}</h1>
+	<Area disabled={state.click1 === 1} numb={state.click0} onClick={handleChange0} onDoubleClick={null}/>
+	<Area disabled={state.click0 === 1} numb={state.click1} onClick={handleChange1}/>
 	</>
 	)
 	}
